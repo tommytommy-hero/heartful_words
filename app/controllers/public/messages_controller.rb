c6c070ff05a4a96eb95ebaaac5e868048f0420b0
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-11-05 05:48:31 +0000

    Add:message:new

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,26 +1,44 @@
 class Public::MessagesController < ApplicationController
 
   def new
+    @message = Message.new
+    @tags = Tag.all
   end
 
   def create
-  end
-
-  def index
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    if @message.save
+      redirect_to user_path(current_user)
+    else
+      @tags = Tag.all
+      render "new"
+    end
   end
 
   def edit
+    @message = Message.find(params[:id])
+    @tags = Tag.all
   end
 
   def update
+    @message = Message.find(params[:id])
+    if @message.update(message_params)
+      redirect_to user_path(current_user)
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to request.referer
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:user_id, :name, :tag_id)
+    params.require(:message).permit(:user_id, :text, :tag_id)
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,7 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
+    @messages = @user.messages
   end
 
   def edit
