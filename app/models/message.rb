8ca8ffd1871af1b45f482979aa361813deabc821
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-11-08 02:49:12 +0000

    Add:favorite

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :tag, optional: true
+  has_many :favorites, dependent: :destroy
 
+  def favorited?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
 
 
