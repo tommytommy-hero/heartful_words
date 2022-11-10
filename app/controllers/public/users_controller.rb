310d81602f4ef290bfab6c9ae59be4cb73a45496
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-11-10 03:10:47 +0000

    Add:mylist

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,4 +8,10 @@ class Public::UsersController < ApplicationController
 
   def edit
   end
+
+  def favorites
+    @user = User.find(params[:id])
+    favorites = Favorite.where(user_id: @user.id).pluck(:message_id)
+    @messages = Message.where(id: favorites).order("RANDOM()").limit(1)
+  end
 end
