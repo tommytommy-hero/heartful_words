0ecb2bdf30dae9c5cad66d25fea7891e3dab32ad
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-11-14 08:39:32 +0000

    Add:user:edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,11 +7,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
 
   def favorites
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:message_id)
     @messages = Message.where(id: favorites).order("RANDOM()").limit(1)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
